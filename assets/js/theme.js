60996721fb607191017e71e95e3cdaa7ea01c8af
Anirudh Kaushik <anirudhkaushik2003@gmail.com>
2023-09-09 17:43:49 +0530

    set default theme to light

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -90,9 +90,11 @@ let transTheme = () => {
 let initTheme = (theme) => {
   if (theme == null || theme == "null") {
     const userPref = window.matchMedia;
-    if (userPref && userPref("(prefers-color-scheme: dark)").matches) {
-      theme = "dark";
-    }
+    // if (userPref && userPref("(prefers-color-scheme: dark)").matches) {
+    //   theme = "light";
+    // }
+    theme = "light";
+
   }
 
   setTheme(theme);
